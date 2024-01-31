7eb562342cf6b6431d9484ecade87630fdfc6808
Jagdeep Singh <jagdeepsingh1630@gmail.com>
2024-01-31 11:05:42 +0530

    save

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,6 @@
 const bcrypt = require('bcrypt');
 const userModel = require('../models/userModel.js');
 const { createToken } = require('./JwtToken.js');
-// exports.addUser = async(req, res) => {
-// 	// console.log(req.body);
-// 	let data = { ...req.body };
-//     console.log("method start");
-//     let cityid,stateid,countryid;
-
-//     let country = await dbcon.promise().query(`INSERT INTO COUNTRY(countryName) VALUES('${data.country}')`);
-//     countryid = country[0].insertId;
-    
-//     let state = await dbcon.promise().query(`INSERT INTO STATE(stateName,countryID) VALUES('${data.state}',${countryid})`);
-//     stateid = state[0].insertId;
-    
-//     let city = await dbcon.promise().query(`INSERT INTO CITY(cityName,stateID) VALUES('${data.city}',${stateid})`)
-//     cityid = city[0].insertId;
-
-// 	let cust = await dbcon.promise().query(
-// 		`INSERT INTO CUSTOMER(firstName,lastName,phone,email,address1,address2,postalCode,cityID,stateID,countryID) VALUES('${data.firstName}','${data.lastName}','${data.phone}','${data.email}','${data.address1}','${!data.address2 ? "" : data.address2}','${data.pincode}','${cityid}','${stateid}','${countryid}')`);
-//     console.log(cust[0]);
-//     console.log("method end");
-
-//     dbcon.query('select * from customer',(err,results)=>{
-//         console.log(results);
-//     });
-// 	res.send(req.body);
-// }
 
 
 exports.addUser = async(req,res,next)=>{
@@ -36,7 +11,7 @@ exports.addUser = async(req,res,next)=>{
         let result = await userModel.create(data);
 
         req.session.currUser = result;
-        console.log(req.session.currUser);
+        // console.log(req.session.currUser);
 
         createToken(result,res,'User created Successfully',200);
         // res.status(200).json({
@@ -64,7 +39,7 @@ exports.login = async(req,res,next)=>{
 
         // req.session.currUser = user;
         req.session.currUser = user;
-        console.log(req.session.currUser);
+        // console.log(req.session.currUser);
 
         createToken(user,res,"Login Successful",200);
     }catch(err){
