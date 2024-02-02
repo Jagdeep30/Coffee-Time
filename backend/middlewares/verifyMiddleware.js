830f5ec6eac607a8f740497d37093b43153e8311
Jagdeep Singh <js6816316@gmail.com>
2024-02-02 16:25:21 +0530

    32

--- backend/middlewares/verifyMiddleware.js
+++ backend/middlewares/verifyMiddleware.js
@@ -1,57 +1,48 @@
-const jwt = require('jsonwebtoken');
-const { getCurrUser } = require('./config');
+const jwt = require("jsonwebtoken");
+const { getCurrUser } = require("./config");
 
+const verifyReq = (req, res, next) => {
+	try {
+		// console.log("user is ------>>>>>>>~~~~~~~~~~~" +req.session.currUser);
+		let cookie = req.cookies;
 
-const verifyReq = (req,res,next)=>{
-    try{
+		// let user = req.session.currUser;
 
-        // console.log("user is ------>>>>>>>~~~~~~~~~~~" +req.session.currUser);
-        let cookie = req.cookies;
+		let user = getCurrUser();
 
-
-
-        // let user = req.session.currUser;
-
-        let user = getCurrUser();
-        
-        
-        
-        
-        
-        
-        // console.log(user);
-        // console.log(cookie);
-        if(user===undefined || !cookie.token){
-            res.status(200).json({
-                status:'fail',
-                message:`Login first to access data ---> token is ${user}`
-            })
-            // res.redirect("http://localhost:3000/signin");
-            // console.log("CONGRATS YOUR USER IS UNDEFINED");
-        }
-        else{
-            let checkUser = jwt.verify(cookie.token,process.env.JWT_SECRET);
-            console.log("i am in the else statement and this is the data --->"+JSON.stringify(checkUser)+" --> "+user);
-            console.log("this is the checkUser ---> "+checkUser._id==user._id);
-            if(checkUser._id === user._id){
-                next();
-            }
-            else{
-                res.status(200).json({
-                    status:'fail',
-                    message:"login first to continue"
-                })
-            }
-        }
-
-    }catch(err){
-        next(err);
-    }
-
-
-
-
-
-}
+		// console.log(user);
+		// console.log(cookie);
+		if (user === undefined || !cookie.token) {
+			res.status(200).json({
+				status: "fail",
+				message: `Login first to access data ---> token is ${user}`,
+			});
+			// res.redirect("http://localhost:3000/signin");
+			// console.log("CONGRATS YOUR USER IS UNDEFINED");
+		} else {
+			let checkUser = jwt.verify(cookie.token, process.env.JWT_SECRET);
+			console.log(
+				"i am in the else statement and this is the data --->" +
+					JSON.stringify(checkUser) +
+					" --> " +
+					user
+			);
+			console.log(
+				"this is the checkUser ---> " + checkUser._id ==
+					user._id.toString()
+			);
+			if (checkUser._id === user._id) {
+				next();
+			} else {
+				res.status(200).json({
+					status: "fail",
+					message: "login first to continue",
+				});
+			}
+		}
+	} catch (err) {
+		next(err);
+	}
+};
 
 module.exports = verifyReq;
