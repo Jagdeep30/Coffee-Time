2c8ec4b5f6b5fcbeec958facb0d38fa4d0871487
Jagdeep Singh <js6816316@gmail.com>
2024-02-02 16:14:21 +0530

    30

--- backend/middlewares/verifyMiddleware.js
+++ backend/middlewares/verifyMiddleware.js
@@ -31,8 +31,8 @@ const verifyReq = (req,res,next)=>{
         }
         else{
             let checkUser = jwt.verify(cookie.token,process.env.JWT_SECRET);
-            console.log("i am in the else statement and this is the data --->"+process.env.JWT_SECRET+" --> "+cookie.token);
-            console.log("this is the checkUser ---> "+checkUser._id===user._id);
+            console.log("i am in the else statement and this is the data --->"+checkUser+" --> "+user);
+            console.log("this is the checkUser ---> "+checkUser._id==user._id);
             if(checkUser._id === user._id){
                 next();
             }
